2d84b868e3f09e6f4783a3670a7080ce325fa620
huandv <dohuan2k1@gmail.com>
2021-10-04 19:00:16 +0700

    abc

--- src/main/java/com/example/footballshopwebapp/service/SmsService.java
+++ src/main/java/com/example/footballshopwebapp/service/SmsService.java
@@ -18,18 +18,18 @@ import javax.mail.Store;
 
 @Component
 public class SmsService {
-//    private final String ACCOUNT_SID = "ACdac1f9429874bc949adfee5ee63022dc";
+    private final String ACCOUNT_SID = "ACdac1f9429874bc949adfee5ee63022dc";
+
+    private final String AUTH_TOKEN = "70f745a24cd8668a297f34ed1fd8b646";
+
+    private final String FROM_NUMBER = "+17242133653";
+
+
+//    private final String ACCOUNT_SID = "ACec76cbdb19b31d8d747439c756e04032";
 //
-//    private final String AUTH_TOKEN = "194ca6479f40fdc278e293ceead61628";
-//
-//    private final String FROM_NUMBER = "+17242133653";
-
-
-    private final String ACCOUNT_SID = "ACec76cbdb19b31d8d747439c756e04032";
-
 //    private final String AUTH_TOKEN = "ca5b763f5064bb3d8e2cff381ffbe6ce";
-
-    private final String FROM_NUMBER = "+13853991793";
+//
+//    private final String FROM_NUMBER = "+13853991793";
 
     public void send(SmsPojo sms) throws ParseException {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
